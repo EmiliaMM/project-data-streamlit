8e4669f4c7d05ace73fd00b92ccfec54d9efb059
EmiliaMM <91829290+EmiliaMM@users.noreply.github.com>
2023-04-14 23:57:24 +0200

    Update data.py

--- data.py
+++ data.py
@@ -121,3 +121,79 @@ selected = option_menu(
 )
 
 
+#--- TOP CHART's
+
+total_offers = df2["title"].count()
+total_offers_select = df_selection["title"].count()
+
+# Definicja funkcji obliczającej średnią wypłatę
+def avg_salary(df_selection):
+    avg_salary_from = df_selection["salary_from"].mean()
+    avg_salary_to = df_selection["salary_to"].mean()
+    avg_salary = f"{avg_salary_from:.2f} - {avg_salary_to:.2f} PLN"
+    return avg_salary
+
+
+# i
+
+#[BAR CHART]
+
+
+top_cities = df_selection.groupby("city")["title"].count().reset_index()
+top_cities = top_cities.sort_values("title", ascending=False).head(10)
+
+fig1 = px.bar(top_cities, x="city", y="title", title="Top 10 miast z największą ilością ofert pracy")
+# st.plotly_chart(fig1)
+
+
+def draw_map(df_selection):
+    # Ustawienie mapy zgodnie z granicami Polski
+    # mapbox_access_token = "pk.eyJ1IjoibXNhbGFyaXMiLCJhIjoiY2tjN3lqanZtMDlzbTJycWtkaXk1czc1eCJ9.8rt_mjGZfuJwEziXTgHaow"
+    # px.set_mapbox_access_token(mapbox_access_token)
+    fig = px.density_mapbox(df_selection, lat="latitude", lon="longitude", radius=5,
+                            hover_data={"title": True, 
+                                        "city": True, 
+                                        "marker_icon": True, 
+                                        "salary_from": True, "salary_to": True,
+                                        "skill_one": True, "skill_two": True, "skill_three": True},
+                            mapbox_style="carto-darkmatter", center={"lat": 52.237049, "lon": 21.017532}, zoom=5, opacity=0.8)
+    fig.update_traces(hovertemplate="<b>Stanowisko:</b> %{customdata[0]}<br>"
+                                     "<b>Miasto:</b> %{customdata[1]}<br>"
+                                     "<b>Kategoria:</b> %{customdata[2]}<br>"
+                                     "<b>Wypłata:</b> %{customdata[3]}-%{customdata[4]} PLN<br>"
+                                     "<b>Skills:</b> %{customdata[5]}, %{customdata[6]}, %{customdata[7]}<extra></extra><br>"
+                                     )
+    fig.update_layout(hoverlabel=dict(bgcolor="#23272b", font=dict(color="white")))
+    fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
+    fig.update_layout(mapbox=dict(center=dict(lat=52.237049, lon=21.017532), zoom=5, style="carto-darkmatter"))
+    fig.update_layout(coloraxis_showscale=False)
+    fig.update_layout(title=f"Liczba ofert: {len(df_selection)}")
+
+    return fig
+
+
+# Rysowanie mapy
+# st.plotly_chart(draw_map(df_selection))
+if selected == "Dane ogólne":
+    left_column, middle_column, right_coulmn = st.columns(3)
+    with left_column:
+        st.subheader("Wszytskie oferty: ")
+        st.subheader(f"{total_offers:,} ofert")
+    with middle_column:
+        st.subheader("Wyfiltrowane oferty:")
+        st.subheader(f"{total_offers_select:,} ofert")
+    with right_coulmn:
+        st.subheader("Średnia wypłata:")
+        st.subheader(avg_salary(df_selection))
+    st.markdown("---")
+
+    left_column, right_coulmn = st.columns(2)
+    left_column.plotly_chart(fig1, use_container_width=True)
+    with right_coulmn:
+        st.subheader("Rozłożenie ofert na mapie Polski")
+        right_coulmn.plotly_chart(draw_map(df_selection),use_container_width=True)
+
+st.markdown("---")
+
+
+
